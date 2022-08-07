e6c6d6729ba40635815e6cd9b0b61f7fb2a87091
DeadpanSplash82 <u21434159@tuks.co.za>
2022-08-07 19:41:19 +0200

    First working version

--- Prac 2/Filter.java
+++ Prac 2/Filter.java
@@ -35,10 +35,10 @@ public class Filter implements Lock {
 	public Filter(int size) {
 		victim = new HashMap<Integer, String>();
 		index = new HashMap<String, Integer>();
-		level = new Integer[size+1];
-		n = size+1;
+		level = new Integer[size];
+		n = size;
 
-		for (int i = 1; i <= size; i++) {
+		for (int i = 0; i < size; i++) {
 			victim.put(i, "");
 			level[i] = 0;
 		}
@@ -47,13 +47,27 @@ public class Filter implements Lock {
 	@Override
 	public void lock() {
 		addIndex(Thread.currentThread().getName());
-		for (int L = 1; L <= n; L++) {
+		int L = 1;
+		for (L = 1; L <= n - 1; L++) {
 			level[index.get(Thread.currentThread().getName())] = L;
 			victim.put(L, Thread.currentThread().getName());
-			while (higherExists(L) && victim.get(L) == Thread.currentThread().getName()) {
+			// testPrint();
+			while (higherExists(L, Thread.currentThread().getName())
+					&& victim.get(L) == Thread.currentThread().getName()) {
 			}
-
 		}
+		// System.out.println("unblocking " + Thread.currentThread().getName() + " at level "
+		// 		+ level[index.get(Thread.currentThread().getName())] +
+		// 		" where victim is " + victim.get(L) + " and higher exists "
+		// 		+ higherExists(L, Thread.currentThread().getName()));
+
+	}
+
+	private void testPrint() {
+		System.out.println("Thread: " + Thread.currentThread().getName());
+		System.out.println("victims: " + victim.toString());
+		System.out.println("index: " + index.toString());
+		System.out.println("level: " + level.toString());
 	}
 
 	@Override
@@ -61,10 +75,17 @@ public class Filter implements Lock {
 		level[index.get(Thread.currentThread().getName())] = 0;
 	}
 
-	private Boolean higherExists(int curr) {
-		for (String s : index.keySet()) {
-			// System.out.println("Index " + s + ": " + index.get(s));
-			if (level[index.get(s)] > curr) {
+	private Boolean higherExists(int curr, String name) {
+		// for (String s : index.keySet()) {
+		// // System.out.println("Index " + s + ": " + index.get(s));
+		// if (s != name && level[index.get(s)] > curr) {
+		// return true;
+		// }
+		// }
+		// return false;
+
+		for (int t = 0; t < n; t++) {
+			if (t != index.get(name) && level[t] >= curr) {
 				return true;
 			}
 		}
@@ -73,7 +94,7 @@ public class Filter implements Lock {
 
 	private void addIndex(String name) {
 		if (!index.containsKey(name)) {
-			index.put(name, index.size() + 1);
+			index.put(name, index.size());
 		}
 	}
 }
