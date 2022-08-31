231946507773b99c6785f70c083e9f59e539918f
DeadpanSplash82 <u21434159@tuks.co.za>
2022-08-31 11:06:47 +0200

    Added RMWConsensus

---
+++ Prac 3/RMWConsensus.java
@@ -0,0 +1,14 @@
+public class RMWConsensus extends ConsensusProtocol<Integer> {
+
+    public RMWConsensus(int threadCount) {
+        super(threadCount);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void decide() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
