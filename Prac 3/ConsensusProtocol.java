389e0dec4c52698eaa7b51411a8551842dd08252
DeadpanSplash82 <u21434159@tuks.co.za>
2022-08-31 11:15:00 +0200

    Implemented propose function

--- Prac 3/ConsensusProtocol.java
+++ Prac 3/ConsensusProtocol.java
@@ -1,13 +1,18 @@
-public abstract class ConsensusProtocol<T> implements Consensus<T>
-{
+public abstract class ConsensusProtocol<T> implements Consensus<T> {
 	public volatile Object[] proposed;
 
-	public ConsensusProtocol(int threadCount)	{
+	public ConsensusProtocol(int threadCount) {
 		proposed = new Object[threadCount];
 	}
 
-	public void propose(T value){
-		
+	public void propose(T value) {
+		int i = 0;
+		while (i < proposed.length && proposed[i] != null) {
+			i++;
+		}
+		if (i < proposed.length) {
+			proposed[i] = value;
+		}
 	}
 
 	abstract public void decide();
