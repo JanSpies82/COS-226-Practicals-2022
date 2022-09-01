4f22e7646eb72be315fed8a00c933a63037f1fbe
DeadpanSplash82 <u21434159@tuks.co.za>
2022-09-01 14:42:35 +0200

    Added timer

--- Prac 3/Task 2/Main.java
+++ Prac 3/Task 2/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        
+        for (int h = 0; h < 3; h++) {
+            long startTime = System.currentTimeMillis();
+            //////////////////////////////////////
+            long stopTime = System.currentTimeMillis();
+            long elapsedTime = stopTime - startTime;
+            System.out.println(elapsedTime + " ms");
+        }
+
     }
 }
