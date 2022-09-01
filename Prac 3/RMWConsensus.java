e17bd212cd36bc6b2abb1f88c4fea3f5f05f4888
DeadpanSplash82 <u21434159@tuks.co.za>
2022-09-01 11:51:33 +0200

    Added CAS implementation

--- Prac 3/RMWConsensus.java
+++ Prac 3/RMWConsensus.java
@@ -1,5 +1,10 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class RMWConsensus<T> extends ConsensusProtocol<T> {
-    private RMWRegister r = new RMWRegister();
+    // private RMWRegister r = new RMWRegister();
+    private final int FIRST = -1;
+    private AtomicInteger counter = new AtomicInteger(FIRST);
+
     public RMWConsensus(int threadCount) {
         super(threadCount);
     }
@@ -7,7 +12,10 @@ public class RMWConsensus<T> extends ConsensusProtocol<T> {
     @Override
     public T decide(T value) {
         propose(value);
-        
+        if (counter.getAndSet(Integer.parseInt(String.valueOf(Thread.currentThread().getName().charAt(7)))) == FIRST)
+            return (T) proposed[Integer.parseInt(String.valueOf(Thread.currentThread().getName().charAt(7)))];
+        else
+            return (T) proposed[(Integer.parseInt(String.valueOf(Thread.currentThread().getName().charAt(7))) + 1) % 2];
     }
 
 }
