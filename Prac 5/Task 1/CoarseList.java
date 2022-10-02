38a250a5854678673802d3f57451cbd0ec5c2657
DeadpanSplash82 <u21434159@tuks.co.za>
2022-10-02 17:55:32 +0200

    Almost working version of Task 1

--- Prac 5/Task 1/CoarseList.java
+++ Prac 5/Task 1/CoarseList.java
@@ -1,18 +1,28 @@
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-@SuppressWarnings("unchecked")
+@SuppressWarnings({ "unchecked", "rawtypes" })
 public class CoarseList<T> {
     private Node<T> head;
     private Lock lock = new ReentrantLock();
 
+    public static final String RESET = "\033[0m";
+    public static final String BLACK = "\033[0;30m";
+    public static final String RED = "\033[0;31m";
+    public static final String GREEN = "\033[0;32m";
+    public static final String YELLOW = "\033[0;33m";
+    public static final String BLUE = "\033[0;34m";
+    public static final String PURPLE = "\033[0;35m";
+    public static final String CYAN = "\033[0;36m";
+    public static final String WHITE = "\033[0;37m";
+
     public CoarseList() {
-        head = new Node(Integer.MIN_VALUE);
-        head.next = new Node(Integer.MAX_VALUE);
+        head = new Node(Integer.MIN_VALUE, -1, 0);
+        head.next = new Node(Integer.MAX_VALUE, -1, 0);
 
     }
 
-    public boolean add(T item) {
+    public boolean add(T item, int person, long time) {
         Node pred, curr;
         int key = item.hashCode();
         lock.lock();
@@ -26,7 +36,8 @@ public class CoarseList<T> {
             if (key == curr.key)
                 return false;
             else {
-                Node node = new Node(item);
+                Node node = new Node(item, person, time);
+                System.out.println(node.getName() + GREEN + " has been added" + RESET);
                 node.next = curr;
                 pred.next = node;
                 return true;
@@ -40,6 +51,7 @@ public class CoarseList<T> {
         Node pred, curr;
         int key = item.hashCode();
         lock.lock();
+        printList();
         try {
             pred = head;
             curr = pred.next;
@@ -56,6 +68,19 @@ public class CoarseList<T> {
         } finally {
             lock.unlock();
         }
+    }
+
+    private void printList() {
+        Node curr = head.next;
+        String out = RED + "List: " + RESET;
+        if (curr.person != -1)
+            out += "[" + curr.getName() + "]";
+        while (curr.next != null) {
+            curr = curr.next;
+            if (curr.person != -1)
+                out += "-> [" + curr.getName() + "]";
+        }
+        System.out.println(out);
     }
 
 }
--- Prac 5/Task 1/Gallery.java
+++ Prac 5/Task 1/Gallery.java
@@ -1,12 +1,13 @@
+@SuppressWarnings({ "unchecked", "rawtypes", "generics" })
 public class Gallery {
     CoarseList c;
-    
+
     public Gallery() {
         c = new CoarseList();
     }
 
-    public void enter() {
-        c.add(Thread.currentThread());
+    public void enter(int person, long time) {
+        c.add(Thread.currentThread(), person, time);
     }
 
     public void exit() {
